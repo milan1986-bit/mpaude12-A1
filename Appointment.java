7ae70cf9cdc85780b0425db67510db8ae242a106
milan <milan@gmail.com>
2025-07-18 20:48:10 +0500

    Working on the Assignment 1 Part 4 Constructors

--- Appointment.java
+++ Appointment.java
@@ -1,3 +1,16 @@
 public class Appointment {
+    private String patientName;
+    private String patientMobile;
+    private String preferredTimeSlot;
+    private HealthProfessional healthProfessional;
+
+    public Appointment() {}
+
+    public Appointment(String patientName, String patientMobile, String preferredTimeSlot, HealthProfessional healthProfessional) {
+        this.patientName = patientName;
+        this.patientMobile = patientMobile;
+        this.preferredTimeSlot = preferredTimeSlot;
+        this.healthProfessional = healthProfessional;
+    }
     
 }
